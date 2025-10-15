d47439c8eab6cfbedbc1d9d3493b99726b48daed
HarrisonDebz <harrisonmalisawa16@gmail.com>
2025-10-15 20:47:11 +0200

    Added EventModel.cs to represent event data

---
+++ Models/EventModel.cs
@@ -0,0 +1,7 @@
+public class EventModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public string Venue { get; set; }
+}
