f97456166470dd70f0b43b2689acf196c094f329
HarrisonDebz <harrisonmalisawa16@gmail.com>
2026-06-02 21:46:20 +0200

    Notifications implemetation

--- EventPulse_v1/Pages/NotificationsPage.xaml.cs
+++ EventPulse_v1/Pages/NotificationsPage.xaml.cs
@@ -1,12 +1,14 @@
 using Microsoft.Maui.Controls;
+using EventPulse_v1.ViewModels;
 
 namespace EventPulse_v1.Pages
 {
     public partial class NotificationsPage : ContentPage
     {
-        public NotificationsPage()
+        public NotificationsPage(NotificationsViewModel vm)
         {
             InitializeComponent();
+            BindingContext = vm;
         }
     }
 }
--- EventPulse_v1/ViewModels/NotificationsViewModel.cs
+++ EventPulse_v1/ViewModels/NotificationsViewModel.cs
@@ -1,38 +1,23 @@
 using System.Collections.ObjectModel;
-using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace EventPulse_v1.ViewModels
 {
-    public class NotificationsViewModel : BaseViewModel
+    public partial class NotificationsViewModel : BaseViewModel
     {
-        private bool _pushEnabled = true;
-        public bool PushEnabled
-        {
-            get => _pushEnabled;
-            set
-            {
-                _pushEnabled = value;
-                RaisePropertyChanged();
-            }
-        }
+        [ObservableProperty]
+        bool pushEnabled = true;
 
         public ObservableCollection<string> LeadTimeOptions { get; } = new() { "30 minutes", "1 hour", "24 hours" };
 
-        private string _selectedLeadTime = "1 hour";
-        public string SelectedLeadTime
-        {
-            get => _selectedLeadTime;
-            set
-            {
-                _selectedLeadTime = value;
-                RaisePropertyChanged();
-            }
-        }
+        [ObservableProperty]
+        string selectedLeadTime = "1 hour";
 
         public ObservableCollection<Reminder> UpcomingReminders { get; } = new();
 
         public NotificationsViewModel()
         {
+            Title = "Notifications";
             // Seed sample reminders
             UpcomingReminders.Add(new Reminder { EventTitle = "Tech Innovators Meetup", WhenSummary = "Tomorrow at 6:00 PM" });
             UpcomingReminders.Add(new Reminder { EventTitle = "Music Night", WhenSummary = "Next week at 7:00 PM" });
